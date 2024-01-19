b98ba6aa1f1e5b6a33f2acda23b2701f5b8dd976
nheek <nickjameshipol@gmail.com>
2024-01-19 18:27:52 +0100

    litt endring på ting -- font to poppins

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,8 @@ const MainRoot = () => {
   return (
     <>
       <Head>
-        <style>{`
-          * {
-            font-family: "Inter", sans-serif !important;
-          }
-         `}</style>
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Montserrat:ital,wght@0,400;0,600;0,700;1,700&family=Poppins:wght@400;600;700&display=swap" />
         <title>{"<PIN/>"}</title>
-       </Head>
+      </Head>
       <Header/>
       <Hero />
       <Info/>
