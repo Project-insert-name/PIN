c4ccca65e9973dc551efeb749efe763e0100028f
676626 <676626@stud.hvl.no>
2024-01-22 17:28:06 +0100

    logo in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,26 +1,32 @@
 export default function Info () {
     return (
-        <footer className="h-[10vh] bg-sky-800 flex">
+        <footer className="h-[10vh] bg-sky-800 flex justify-center items-center ">
 
-            <div className="w-1/3">
+            <div className="w-1/3 flex justify-center">
                 <nav>
-                    <ul className="flex gap-2">
+                    <ul className="flex gap-6">
                         <li>Blah blah</li>
                         <li>Kontaks oss</li>
                     </ul>
                 </nav>
             </div>
 
-            <div className="w-1/3">
+            <div className="w-1/3 flex justify-center">
                 Copyright 2024
             </div>
 
-            <div className="w-1/3">
-                <ul>
+            <div className="w-1/3 flex justify-center">
+                <ul className="flex flex-column items-center gap-4">
 
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li>
+                        <img src="/user/facebook.svg" alt="Facebook" />
+                    </li>
+                    <li>
+                        <img src="/user/twitter.svg" alt="Twitter" />
+                    </li>
+                    <li>
+                        <img src="/user/insta.svg" alt="Insta" />
+                    </li>
 
                 </ul>
             </div>
