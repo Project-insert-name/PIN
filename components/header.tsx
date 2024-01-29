8d0d1f58c5edd104b319a2603250c15297f4f296
676626 <676626@stud.hvl.no>
2024-01-29 11:22:50 +0100

    buttons

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,6 @@
+
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="w-[95%] mt-2 rounded-3xl fixed z-10 left-1/2 transform -translate-x-1/2 border-l-2 border-t-2 border-sky-400 border-opacity-25 shadow-[15px_20px_0px_0px_rgba(0,0,0,0.1)] h-[12vh] bg-sky-700 px-6 flex items-center ">
@@ -7,26 +10,35 @@ export default function Header() {
       <section className="w-1/2">
         <nav>
           <ul className="flex justify-end gap-4">
-            <li className="flex items-center gap-1">
-              <div>
-                <img src="/user/home.svg" alt="Home icon" />
-              </div>
-              <span>Home</span>
-            </li>
-            <li className="flex items-center gap-1">
-              <div>
-                <img src="/user/contact.svg" alt="Contact icon" />
-              </div>
-              <span>Contact</span>
+            <li >
+              <Link href="/">
+              <button className="flex items-center gap-1">
+                  <div>
+                    <img src="/user/home.svg" alt="Home icon" />
+                  </div>
+                  <span>Home</span>
+                </button>
+              </Link>
+              </li>     
+            <li >
+              <Link href="">
+                <button className="flex items-center gap-1">
+                  <div>
+                    <img src="/user/contact.svg" alt="Contact icon" />
+                  </div>
+                  <span>Contact</span>
+                </button>
+              </Link>
             </li>
             <li>
-              <button  className="group bg-white bg-opacity-25 hover:bg-sky-300 duration-200 flex items-center gap-1 px-6 py-3 rounded-2xl">
-                <div>
-                  <img src="/user/discord.svg" alt="Discord icon" />
-                </div>
-                <span className="group-hover:text-blue-950">Join</span>
-              </button>
-           
+              <Link href="">
+                <button  className="group bg-white bg-opacity-25 hover:bg-sky-300 duration-200 flex items-center gap-1 px-6 py-3 rounded-2xl">
+                  <div>
+                    <img src="/user/discord.svg" alt="Discord icon" />
+                  </div>
+                  <span className="group-hover:text-blue-950">Join</span>
+                </button>
+              </Link>
             </li>
           </ul>
         </nav>
