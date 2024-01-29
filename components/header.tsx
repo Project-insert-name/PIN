8b7886fee6fe42c18b4e4e53390d7bbd3f85132a
676626 <676626@stud.hvl.no>
2024-01-29 10:32:45 +0100

    test

--- components/header.tsx
+++ components/header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
               <div>
                 <img src="/user/home.svg" alt="Home icon" />
               </div>
-              <span>Homeeeeeeeee</span>
+              <span>Home</span>
             </li>
             <li className="flex items-center gap-1">
               <div>
